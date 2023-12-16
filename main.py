a8854c9b2eb0edbc80cab40832a6683214f4cbf0
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2023-12-16 21:25:12 +0000

    base example

---
+++ main.py
@@ -0,0 +1,95 @@
+"""
+file_name = main.py
+Created On: 2023/12/16
+Lasted Updated: 2023/12/16
+Description: _FILL OUT HERE_
+Edit Log:
+2023/12/16
+    - Created file
+"""
+
+# STANDARD LIBRARY IMPORTS
+from typing import Final
+...
+
+# THIRD PARTY LIBRARY IMPORTS
+from fastapi import APIRouter
+...
+
+# LOCAL LIBRARY IMPORTS
+...
+
+
+router: Final[APIRouter] = APIRouter
+
+
+
+# Seperate Router Example 
+""" main.py 
+
+from fastapi import Depends, FastAPI
+
+from .dependencies import get_query_token, get_token_header
+from .internal import admin
+from .routers import items, users
+
+app = FastAPI(dependencies=[Depends(get_query_token)])
+
+
+app.include_router(users.router)
+app.include_router(items.router)
+app.include_router(
+    admin.router,
+    prefix="/admin",
+    tags=["admin"],
+    dependencies=[Depends(get_token_header)],
+    responses={418: {"description": "I'm a teapot"}},
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Bigger Applications!"}
+    
+"""
+
+"""items.py 
+from fastapi import APIRouter, Depends, HTTPException
+
+from ..dependencies import get_token_header
+
+router = APIRouter(
+    prefix="/items",
+    tags=["items"],
+    dependencies=[Depends(get_token_header)],
+    responses={404: {"description": "Not found"}},
+)
+
+
+fake_items_db = {"plumbus": {"name": "Plumbus"}, "gun": {"name": "Portal Gun"}}
+
+
+@router.get("/")
+async def read_items():
+    return fake_items_db
+
+
+@router.get("/{item_id}")
+async def read_item(item_id: str):
+    if item_id not in fake_items_db:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return {"name": fake_items_db[item_id]["name"], "item_id": item_id}
+
+
+@router.put(
+    "/{item_id}",
+    tags=["custom"],
+    responses={403: {"description": "Operation forbidden"}},
+)
+async def update_item(item_id: str):
+    if item_id != "plumbus":
+        raise HTTPException(
+            status_code=403, detail="You can only update the item: plumbus"
+        )
+    return {"item_id": item_id, "name": "The great Plumbus"}
+"""
