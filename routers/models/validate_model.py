b4efa7f172ab66a22d3a74544e7496e6ef02ea2f
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2023-12-22 04:29:17 +0000

    models

---
+++ routers/models/validate_model.py
@@ -0,0 +1,24 @@
+"""
+file_name = validate_model.py
+Created On: 2023/12/21
+Lasted Updated: 2023/12/21
+Description: _FILL OUT HERE_
+Edit Log:
+2023/12/21
+    - Created file
+"""
+
+# STANDARD LIBRARY IMPORTS
+...
+
+# THIRD PARTY LIBRARY IMPORTS
+from pydantic import BaseModel
+...
+
+# LOCAL LIBRARY IMPORTS
+...
+
+
+class TokenParam(BaseModel):
+    username: str 
+    password: str
